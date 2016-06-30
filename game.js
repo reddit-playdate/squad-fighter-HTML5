5b452fd13803936c3e5386b314ecdf3433eaef5c
Eric Phillips <ericj.phillips@gmail.com>
2016-06-30 16:41:22 -0400

    initial commit

---
+++ game.js
@@ -0,0 +1,15 @@
+const Phaser = require('phaser')
+
+let game = new Phaser.game(640, 320, Phaser.AUTO)
+
+let state = {
+  preload: function () {
+
+  },
+  create: function () {
+
+  },
+  update: function () {
+    
+  }
+}
